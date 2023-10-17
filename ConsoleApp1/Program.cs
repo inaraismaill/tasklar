8f08ccddb5f9c9dc617b83c610dd70ba17b83524
inaraismaill <mi7q3xpdl@code.edu.az>
2023-10-17 16:06:04 +0400

    task kocuruldu

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿//Ədədin sadə və ya mürəkkəb olduğunu tapan alqoritm yazın.
+
+//int num = 9;
+//bool simple = true;
+
+//for (int i = 2; i < num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        simple = false;
+//        break;
+//    }
+//}
+//if (num <= 1)
+//{
+//    Console.WriteLine("Daxil olunan eded sade ve ya murekkeb deyil.");
+//}
+//else if (simple)
+//{
+//    Console.WriteLine("Daxil olunan eded sadedir.");
+//}
+//else
+//{
+//    Console.WriteLine("Daxil olunan eded murekkebdir.");
+//}
+
+
+//=========================================================================================================
+//Daxil olunan ədədin 2nin qüvvəti olub olmadığını tapan alqoritm yazın.
+
+//int numb = 1;
+//bool answer = false;
+//while (numb>1)
+//{
+//    if (numb%2==1)
+//    {
+//        answer=false; break;    
+//    }
+//    numb /= 2;
+//    answer = true;
+//}
+//if (answer)
+//{
+//    Console.WriteLine("Daxil olunan eded 2 nin quvvetidir.");
+//}
+//else
+//{
+//    Console.WriteLine("Daxil olunan eded 2 nin quvveti deyil.");
+//}
+
+
+//=========================================================================================================
+//Ədədin mərtəbələri sayını tapan alqoritm yazın.
+
+//int number = 1234567898;
+//int floor = 0;
+
+//while (number != 0)
+//{
+//    floor++;
+//    number /= 10;
+//}
+//Console.WriteLine("Daxil etdiyiniz reqem " + floor + " mertebelidir.");
+
+//bubbl sort
